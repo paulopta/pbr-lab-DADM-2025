fb8edc686fc6bb12284142006ec2534968d01ae6
Paulo Henrique Rodrigues <paulohenriquerodrigues@outlook.com>
2025-08-17 16:57:21 +0000

    exemplo image

---
+++ react-native/exemplo-image/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Imagens com Resposividade</Text>
+      {/* Imagem Local*/}
+      <Image
+        source={require('./src/img/exemplo.avif')}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      {/* Imagem remota*/}
+      <Image
+        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      {/* Imagem base 64*/}
+      <Image
+        source={{
+          uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+        }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default App;
